2aabfbb3e6201756e85c0c2c0dd171047886ceee
Jason Lin <2linj24@gmail.com>
2021-04-02 23:44:24 -0500

    Remove object-oriented pipeline notebook

--- ypp/preprocessing/crop.py
+++ ypp/preprocessing/crop.py
@@ -1,26 +1,15 @@
-def invert(self, display=True):
-    self.data = ((1 << self.bits) - 1) - self.data
+import numpy as np
+from PIL import Image
+
+from typing import Tuple
 
 
-def crop(self, center, radius, display=True):
+def invert(data: np.ndarray) -> np.ndarray:
+    datatype = data.dtype
+
+    data = ((1 << 16) - 1) - data
+
+
+def crop(data: np.ndarray, center: Tuple(int, int), radius: float) -> np.ndarray:
     x = center[0]
     y = center[1]
-    self.crop_center = center
-    self.crop_radius = radius
-    cutout = Cutout2D(
-        self.data_original, center, 2 * radius, WCS(self.hdul[0].header), copy=True
-    )
-    self.data = cutout.data
-    self.wcs = cutout.wcs
-    self.center = (x - self.center[0], y - self.center[1])
-    self.wcs_bounds = self.wcs.all_pix2world(
-        np.asarray(
-            [
-                (0, 0),
-                (len(self.data[0]), 0),
-                (0, len(self.data)),
-                (len(self.data[0]), len(self.data)),
-            ]
-        ),
-        1,
-    )
