2670f7632866d76c93e39ad192b4d998b5bd44ad
Jason Lin <2linj24@gmail.com>
2021-04-03 22:47:26 -0500

    Fix cropping

--- ypp/preprocessing/crop.py
+++ ypp/preprocessing/crop.py
@@ -5,11 +5,22 @@ from typing import Tuple
 
 
 def invert(data: np.ndarray) -> np.ndarray:
-    datatype = data.dtype
+    if data.dtype is np.int8:
+        bits = 8
+    elif data.dtype is np.int16:
+        bits = 16
+    else:
+        raise TypeError("Image data is not 8-bit or 16-bit.")
 
-    data = ((1 << 16) - 1) - data
+    return ((1 << bits) - 1) - data
 
 
-def crop(data: np.ndarray, center: Tuple(int, int), radius: float) -> np.ndarray:
-    x = center[0]
-    y = center[1]
+def crop(data: np.ndarray, center: Tuple(int, int), radius: int) -> np.ndarray:
+    max_row, max_col = data.shape
+    cen_row, cen_col = center
+    row_lower_bound = max(cen_row - radius, 0)
+    row_upper_bound = min(cen_row + radius, max_row)
+    col_lower_bound = max(cen_col - radius, 0)
+    col_upper_bound = min(cen_col + radius, max_col)
+
+    return data[row_lower_bound:row_upper_bound, col_lower_bound:col_upper_bound]
