58081f030fc255acfba5f2ca97db28e47fbb33aa
Jason Lin <2linj24@gmail.com>
2021-06-01 23:04:35 -0500

    Fits formatting

--- ypp/utils/fits.py
+++ ypp/utils/fits.py
@@ -1,6 +1,7 @@
 import numpy as np
 from astropy.io import fits
 
+
 def ndarray_to_fits(data: np.ndarray, filename: str):
     hdu = fits.PrimaryHDU(data=data)
     hdu.writeto(filename, overwrite=True)
