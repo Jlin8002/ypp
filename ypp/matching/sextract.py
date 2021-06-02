b51e313bd54d96862c24438730957671c57953ba
Jason Lin <2linj24@gmail.com>
2021-06-02 13:47:55 -0500

    SEP aperture photometry function

--- ypp/matching/sextract.py
+++ ypp/matching/sextract.py
@@ -7,23 +7,72 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 
 
-def sextract_pix_locs(data: np.ndarray, sigma: float):
+def sextract_bkg(data):
     data = data.astype("float")
     bkg = sep.Background(data)
     data = data - bkg
+    return data, bkg
+
+
+def sextract_objects(data: np.ndarray, sigma: float, bkg=None):
+    if bkg is None:
+        data, bkg = sextract_bkg(data)
     objects = sep.extract(data, sigma, err=bkg.globalrms)
-    return np.array([objects["x"], objects["y"]]).T
+    return objects
 
 
-def pix_to_wcs(pix_locs, wcs: WCS):
+def pix_to_wcs(objects, wcs: WCS):
+    pix_locs = np.array([objects["x"], objects["y"]]).T
     object_coords = wcs.pixel_to_world_values(pix_locs)
     return SkyCoord(object_coords, unit=u.deg, frame="icrs")
 
 
 def sextract_world_locs(data, sigma, wcs):
-    object_locs = sextract_pix_locs(data, sigma)
-    object_coords = pix_to_wcs(object_locs, wcs)
+    objects = sextract_objects(data, sigma)
+    object_coords = pix_to_wcs(objects, wcs)
     return object_coords
+
+
+def sextract_magnitudes(
+    data,
+    aperture_radius,
+    buffer_radius,
+    background_radius,
+    bkg=None,
+    objects=None,
+    sigma=3.0,
+):
+    aperture_area = np.pi * (aperture_radius ** 2)
+    buffer_area = np.pi * (buffer_radius ** 2)
+    background_area = (np.pi * (background_radius ** 2)) - buffer_area
+
+    if bkg is None:
+        data, bkg = sextract_bkg(data)
+    if objects is None:
+        objects = sextract_objects(data, sigma, bkg)
+
+    aperture_list, _, _ = sep.sum_circle(
+        data, objects["x"], objects["y"], aperture_radius, err=bkg.globalrms, gain=1.0
+    )
+    background_list, _, _ = sep.sum_circann(
+        data,
+        objects["x"],
+        objects["y"],
+        buffer_radius,
+        background_radius,
+        err=bkg.globalrms,
+        gain=1.0,
+    )
+    background_densities = [
+        annulus_sum / background_area for annulus_sum in background_list
+    ]
+    fluxes = np.array(
+        [
+            aperture_list[i] - (aperture_area * background_densities[i])
+            for i in range(len(aperture_list))
+        ]
+    )
+    return fluxes
 
 
 # sep_table = Table()
